5f90c8171ac0dcfb801c93c5d68dbd11e61d065f
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-12-21 20:14:48 +0300

    Switches task added

---
+++ Assets/Scripts/SwitchTask.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchTask : Task
+{
+    [SerializeField] private Button[] Switches;
+    [SerializeField] private string[] Settings; // Параметры переключения каждого свитча
+    [SerializeField, Tooltip("Debug Value")] private int[] State;
+    [SerializeField] private Color ColorEnabled;
+    [SerializeField] private Color ColorDisabled;
+
+    public void Switch(int i)
+    {
+        for(int _ = 0; _ < Switches.Length; _++)
+        {
+            if (Settings[i][_] == '1')
+                Invert(_);
+        }
+    }
+    private void Invert(int index)
+    {
+        State[index] = 1 - State[index];
+        Switches[index].image.color = State[index] == 1 ? ColorEnabled : ColorDisabled;
+
+        int sum = 0;
+        foreach (int a in State) sum += a;
+        if (sum == 6) Pass();
+    }
+
+
+    private void GetExit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsPassed = false;
+            Close();
+        }
+    }
+    private void Update()
+    {
+        GetExit();
+    }
+
+
+    protected override void Pass()
+    {
+        StatusImage.color = PassColor;
+        IsPassed = true;
+        Close();
+    }
+    protected override void Fail()
+    {
+        StatusImage.color = FailColor;
+        IsPassed = false;
+    }
+
+    public override void Open()
+    {
+        WindowPanel.SetActive(true);
+        PlayerMovement.instance.UseMotor = false;
+    }
+    public override void Close()
+    {
+        WindowPanel.SetActive(false);
+        PlayerMovement.instance.UseMotor = true;
+    }
+}
